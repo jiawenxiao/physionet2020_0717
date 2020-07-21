ddcaa1aaa6b2a6378d8a659a67f886ff41740862
Wenxiao Jia <1950461210@qq.com>
2020-07-21 13:29:16 +0800

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     #在第几个epoch进行到下一个state,调整lr
     stage_epoch = [8]
     #训练时的batch大小
-    batch_size = 128
+    batch_size = 64
     #label的类别数
     num_classes = 27
     #最大训练多少个epoch
